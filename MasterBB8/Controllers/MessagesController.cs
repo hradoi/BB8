566eadf0f73e9232ee48f4971e7ba8f34e64827d
horia <horia.radoi@gmail.com>
2016-07-28 12:34:39 +0300

    init

--- MasterBB8/Controllers/MessagesController.cs
+++ MasterBB8/Controllers/MessagesController.cs
@@ -26,7 +26,7 @@ namespace MasterBB8
                 int length = (activity.Text ?? string.Empty).Length;
 
                 // return our reply to the user
-                Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
+                Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters long.");
                 await connector.Conversations.ReplyToActivityAsync(reply);
             }
             else
