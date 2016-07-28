cc0ce534283b38231b121f08581fa11caf2653ee
PGCaesar <cosmin.posirca@uipath.com>
2016-07-28 18:13:43 +0300

    Crawler LIb Added
    Crawler Project

---
+++ Crawler/Stores/Sector.cs
@@ -0,0 +1,18 @@
+﻿using Crawler.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StorageYard.Manager;
+
+namespace Crawler.Stores
+{
+    public class Sector : Store
+    {
+        public void UpdateDB()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Crawler/Stores/Yellow.cs
@@ -0,0 +1,23 @@
+﻿using Crawler.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StorageYard.Manager;
+using StorageYard.Data;
+
+namespace Crawler.Stores
+{
+    public class Yellow : Store
+    {
+        public void UpdateDB()
+        {
+            Menu m = OrderManager.Instance.Create("Yellow", true, true);
+            Item i = OrderManager.Instance.Create("supa", 12.7, "test",m,true);
+
+
+            OrderManager.Instance.Save();
+        }
+    }
+}
