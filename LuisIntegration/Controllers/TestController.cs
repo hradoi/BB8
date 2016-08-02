c19b2abbdf8152e86f9944727f90d707be724b2c
horia <horia.radoi@gmail.com>
2016-08-02 17:27:17 +0300

    Added L

---
+++ LuisIntegration/Controllers/TestController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Bot_Application2
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        public virtual async Task<HttpResponseMessage> Post([FromBody] Activity activity)
+        {
+            // check if activity is of type message
+            if (activity != null && activity.GetActivityType() == ActivityTypes.Message)
+            {
+                await Conversation.SendAsync(activity, () => new EchoDialog());
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            return new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
+        }
+
+
+        [LuisModel("650bfbb2-eec4-4d51-a197-7a800d21f1a9", "405e762ea3b840e6a415b11731e26c08")]
+        [Serializable]
+        public class EchoDialog : LuisDialog<object>
+        {
+            [LuisIntent("")]
+            public async Task None(IDialogContext context, LuisResult result)
+            {
+                await context.PostAsync("I'm sorry. I didn't understand you.");
+                context.Wait(MessageReceived);
+            }
+
+            [LuisIntent("order food")]
+            public async Task OrderFood(IDialogContext context, LuisResult result)
+            {
+                await context.PostAsync("Here is what I'm going to search for:");
+                var entities = new List<EntityRecommendation>(result.Entities);
+                foreach (var entity in entities)
+                    await context.PostAsync($"{entity.Entity} of type {entity.Type}");
+                context.Wait(MessageReceived);
+            }
+
+            [LuisIntent("remove")]
+            public async Task RemoveItem(IDialogContext context, LuisResult result)
+            {
+                await context.PostAsync("Here is what I will remove:");
+                var entities = new List<EntityRecommendation>(result.Entities);
+                foreach (var entity in entities)
+                    await context.PostAsync($"{entity.Entity} of type {entity.Type}");
+                context.Wait(MessageReceived);
+            }
+
+            [LuisIntent("show menu")]
+            public async Task ShowMenu(IDialogContext context, LuisResult result)
+            {
+                await context.PostAsync("Here is the menu I'll show:");
+                var entities = new List<EntityRecommendation>(result.Entities);
+                foreach (var entity in entities)
+                    await context.PostAsync($"{entity.Entity} of type {entity.Type}");
+                context.Wait(MessageReceived);
+            }
+
+            [LuisIntent("show order")]
+            public async Task ShowOrder(IDialogContext context, LuisResult result)
+            {
+                await context.PostAsync("Here are your entities:");
+                var entities = new List<EntityRecommendation>(result.Entities);
+                foreach (var entity in entities)
+                    await context.PostAsync($"{entity.Entity} of type {entity.Type}");
+                context.Wait(MessageReceived);
+            }
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+} 
---
+++ MasterBB8/Controllers/LuisIntegration.cs
@@ -0,0 +1,65 @@
+﻿//using Microsoft.Bot.Builder.Dialogs;
+//using Microsoft.Bot.Builder.Luis;
+//using Microsoft.Bot.Builder.Luis.Models;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using System.Web;
+
+//namespace MasterBB8.Controllers
+//{
+//    [LuisModel("14ca534c-a2c0-45b9-8f9e-59c418594f09", "405e762ea3b840e6a415b11731e26c08")]
+//    [Serializable]
+//    public class FoodBotDialog : LuisDialog<object>
+//    {
+//        //public const string Entity_location = "Location";
+
+//        [LuisIntent("")]
+//        public async Task None(IDialogContext context, LuisResult result)
+//        {
+//            string message = $"Sorry I did not understand: " + string.Join(", ", result.Intents.Select(i => i.Intent));
+//            await context.PostAsync(message);
+//            context.Wait(MessageReceived);
+//        }
+
+//        //enum City { Paris, London, Seattle, Munich };
+
+//        //[LuisIntent("GetWeather")]
+//        //public async Task GetWeather(IDialogContext context, LuisResult result)
+//        //{
+//        //    var obj = (IEnumerable<City>)Enum.GetValues(typeof(City));
+//        //    EntityRecommendation location;
+
+//        //    if (!result.TryFindEntity(Entity_location, out location))
+//        //    {
+//        //        PromptDialog.Choice(context, SelectCity, City, "In which city do you want to know the weather forecast?");
+//        //    }
+//        //    else
+//        //    {
+//        //        //Add code to retrieve the weather
+//        //        await context.PostAsync($"The weather in {location} is ");
+//        //        context.Wait(MessageReceived);
+//        //    }
+//        //}
+
+//        //private async Task SelectObject(SelectCity context, IAwaitable<City> city)
+//        //{
+//        //    var message = string.Empty;
+//        //    switch (await city)
+//        //    {
+//        //        case City.Paris:
+//        //        case City.London:
+//        //        case City.Seattle:
+//        //        case City.Munich:
+//        //            message = $"The weather in {city} is ";
+//        //            break;
+//        //        default:
+//        //            message = $"Sorry!! I don't have know the weather in {city}";
+//        //            break;
+//        //    }
+//        //    await context.PostAsync(message);
+//        //    context.Wait(MessageReceived);
+//        //}
+//    }
+//}
